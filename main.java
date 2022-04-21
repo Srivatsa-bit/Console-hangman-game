75149e110348bf63c7088c4bdfbb48b2989df762
Srivatsa Rao <58118492+Srivatsa-bit@users.noreply.github.com>
2022-04-21 07:26:13 -0500

    Update main.java

--- main.java
+++ main.java
@@ -2,26 +2,49 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        String gameWord = "Hello";
+    static void gameMethod(String gameWord) {
         String checkWord = "";
+        gameWord = gameWord.toLowerCase();
+        int counter = 0;
+        boolean checker = false;
 
         for (int x = 0; x < gameWord.length(); x++) {
             checkWord += "_";
         }
-        System.out.println(checkWord);
-
-        while (checkWord != gameWord) {
+        while (counter<6 && !checkWord.equals(gameWord)) {
             Scanner scanner = new Scanner(System.in);
 
-            System.out.print("Enter a letter to guess: ");
-            CharSequence guessChar = scanner.next();
+            System.out.print("Enter a letter: ");
+            char guessChar = scanner.nextLine().charAt(0);
 
-            for (int i = 0; i < gameWord.length(); i++) {
-                if (gameWord.contains(guessChar) == true) {
-                    checkWord.substring(i, i+1) = (String)guessChar;
+            for (int x = 0; x < gameWord.length(); x++) {
+                if (guessChar == gameWord.charAt(x)) {
+                    checkWord = checkWord.substring(0, x) + guessChar + checkWord.substring(x + 1);
+                    checker = true;
                 }
             }
+            if (checker) {
+                if (!checkWord.equals(gameWord))
+                System.out.println(checkWord);
+            }
+            else {
+                counter++;
+                System.out.println("The letter you entered was not present in the word. Try again.");
+                System.out.println("You have " + (6-counter) + " guesses remaining.");
+
+            }
+            checker = false;
         }
+        if (counter == 6) {
+            System.out.println("You ran out of guesses. The word was " + gameWord);
+        }
+        else {
+            System.out.println("Congratulations, you win.");
+            System.out.println("The word was " + gameWord);
+        }
+    }
+
+    public static void main(String[] args) {
+        gameMethod("Hello");
     }
 }
